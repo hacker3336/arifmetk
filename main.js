484b3de45b10fd48bb72aed83dad88336e09a566
Your Name <you@example.com>
2023-10-21 15:51:03 +0500

    uy ishi

---
+++ main.js
@@ -0,0 +1,118 @@
+let a = 10;
+let b = 3;
+
+
+let c = a+b;
+let d = a*b;
+let e = a/b;
+let f = a-b;
+let g = a**b;
+let h = a%b;
+
+console.log(a, b, c, d, e, f)
+
+
+let a2 = 7;
+let b2 = 4;
+
+
+let c2 = a2+b2;
+let d2 = a2*b2;
+let e2 = a2/b2;
+let f2 = a2-b2;
+let g2 = a2**b2;
+let h2 = a2%b2;
+
+console.log(a2, b2, c2, d2, e2, f2)
+
+let a3 = 16;
+let b3 = 4;
+
+
+let c3 = a3+b3;
+let d3 = a3*b3;
+let e3 = a3/b3;
+let f3 = a3-b3;
+let g3 = a3**b3;
+let h3 = a3%b3;
+
+console.log(a3, b3, c3, d3, e3, f3)
+
+let a4 = 8;
+let b4 = 3;
+
+
+let c4= a4+b4;
+let d4 = a4*b4;
+let e4 = a4/b4;
+let f4 = a4-b4;
+let g4 = a4**b4;
+let h4 = a4%b4;
+
+console.log(a4, b4, c4, d4, e4, f4)
+
+let a5 = 9;
+let b5 = 3;
+ 
+
+let c5 = a5+b5;
+let d5= a5*b5;
+let e5 = a5/b5;
+let f5 = a5-b5;
+let g5= a5**b5;
+let h5 = a5%b5;
+
+console.log(a, b, c, d, e, f)
+
+let a6 = 86;
+let b6 = 9;
+
+
+let c6 = a6+b6;
+let d6 = a6*b6;
+let e6 = a6/b6;
+let f6 = a6-b6;
+let g6 = a6**b6;
+let h6 = a6%b6;
+
+console.log(a6, b6, c6, d6, e6, f6)
+
+let a7 = 45;
+let b7 = 3;
+
+
+let c7 = a7+b7;
+let d7 = a7*b7;
+let e7 = a7/b7;
+let f7 = a7-b7;
+let g7 = a7**b7;
+let h7 = a7%b7;
+
+console.log(a7, b7, c7, d7, e7, f7)
+
+let a8 = 23;
+let b8 = 2;
+
+
+let c8 = a8+b8;
+let d8 = a8*b8;
+let e8 = a8/b8;
+let f8 = a8-b8;
+let g8 = a8**b8;
+let h8 = a8%b8;
+
+console.log(a8, b8, c8, d8, e8, f8)
+
+
+let a9 = 78;
+let b9 = 7;
+
+
+let c9 = a9+b9;
+let d9 = a9*b9;
+let e9 = a9/b9;
+let f9 = a9-b9;
+let g9 = a9**b9;
+let h9 = a9%b9;
+
+console.log(a9, b9, c9, d9, e9, f9)
